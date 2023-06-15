3989c30aeced6318b00001b948a325ced00f713d
kace2024 <kacemiabdallah01@gmail.com>
2023-06-15 12:55:11 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,7 @@
-#include <stdio.h>
-int main(void) {
-/**
- *Description: main - Prints out a string to stdout.
- *Return: 0 if success.
- */ 
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+#include <stdlib.h>
+
+int main(void)
+{
+    puts("Programming is like building a multilingual puzzle");
+    return 0;
 }
