0859950261bd920c6b85e3ae579a5e7b91220806
kace2024 <kacemiabdallah01@gmail.com>
2023-06-16 16:05:06 -0700

    4. When I was having that alphabet soup, I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+/*
+ * File: 4-print_alphabt.c
+ * Auth: KACEMI ABDALLAH
+ */
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase, followed by a new line.
+ *
+ * Return: Always 0.
+*/
+        int main(void)
+{
+    char letter;  /* variable to store each letter of the alphabet */
+
+    for (letter = 'a'; letter <= 'z'; letter++)
+    {
+        if (letter != 'q' && letter != 'e') /* only print the letter if it's not q or e */
+        {
+            putchar(letter); /* print the letter */
+        }
+    }
+    putchar('\n'); /* print a newline character at the end */
+    return 0;
+}
