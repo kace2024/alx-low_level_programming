31709b66994a1626e56a286b36d25a531758736c
kace2024 <kacemiabdallah01@gmail.com>
2023-06-16 16:39:53 -0700

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+/*
+ * File: 6-print_numberz.c
+ * Auth: KACEMI ABDALLAH
+ */
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base 10 starting from 0, followed by a new line.
+ *
+ * Return: Always 0.
+*/
+int main(void)
+{
+	 int i;
+
+	 for (i = 0; i < 10; i++)
+	 {
+		 putchar(i + '0');
+	 }
+
+	 putchar('\n');
+
+	 return (0);
+}
