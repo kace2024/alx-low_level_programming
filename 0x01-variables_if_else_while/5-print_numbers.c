d6cf23110a3054b13a2bb9f9efb389fe7e2a428e
kace2024 <kacemiabdallah01@gmail.com>
2023-06-16 16:33:31 -0700

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+/*
+ * File: 5-print_numbers.c
+ * Auth: KACEMI ABDALLAH
+ */
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+
+	printf("\n");
+	return (0);
+}
