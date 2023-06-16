58c3a8ab49cf8712545e995e1387505978ddc4a6
kace2024 <kacemiabdallah01@gmail.com>
2023-06-16 16:59:17 -0700

    6. Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -11,12 +11,12 @@
 
 int main(void)
 {
-	 int i;
+       int i;
 
-	 for (i = 0; i < 10; i++)
-		 putchar((i % 10) + '0');
-		
-	 putchar('\n');
+       for (i = 0; i < 10; i++)
+	       putchar((i % 10) + '0');
 
-	 return (0);
+       putchar('\n');
+
+       return (0);
 }
