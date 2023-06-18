d4f5eb219768dfba4bf4aae09e5d4a62eda0a578
kace2024 <kacemiabdallah01@gmail.com>
2023-06-18 05:24:42 -0700

    12. Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,4 +1,13 @@
+/*
+ * File: 102-print_comb5.c
+ * Auth: KACEMI ABDALLAH
+ */
 #include <stdio.h>
+/**
+ * main - Prints all possible combinations of two two-digit numbers.
+ *
+ * Return: Always 0.
+*/
 
 int main(void)
 {
