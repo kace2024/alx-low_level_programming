6f453984bfeecda3ff8aa954b38fffed5fed4ad4
kace2024 <kacemiabdallah01@gmail.com>
2023-06-21 14:49:55 -0700

    16. In computer class,

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,47 +7,47 @@
  */
 int main(void)
 {
-    int count;
-    unsigned long fib1 = 0, fib2 = 1, sum;
-    unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
-    unsigned long half1, half2;
+	int count;
+	unsigned long fib1 = 0, fib2 = 1, sum;
+	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
+	unsigned long half1, half2;
 
-    for (count = 0; count < 92; count++)
-    {
-        sum = fib1 + fib2;
-        printf("%lu, ", sum);
-        fib1 = fib2;
-        fib2 = sum;
-    }
+	for (count = 0; count < 92; count++)
+	{
+		sum = fib1 + fib2;
+		printf("%lu, ", sum);
+		fib1 = fib2;
+		fib2 = sum;
+	}
 
-    fib1_half1 = fib1 / 10000000000UL;
-    fib2_half1 = fib2 / 10000000000UL;
-    fib1_half2 = fib1 % 10000000000UL;
-    fib2_half2 = fib2 % 10000000000UL;
+	fib1_half1 = fib1 / 10000000000UL;
+	fib2_half1 = fib2 / 10000000000UL;
+	fib1_half2 = fib1 % 10000000000UL;
+	fib2_half2 = fib2 % 10000000000UL;
 
-    for (count = 93; count < 99; count++)
-    {
-        half1 = fib1_half1 + fib2_half1;
-        half2 = fib1_half2 + fib2_half2;
+	for (count = 93; count < 99; count++)
+	{
+		half1 = fib1_half1 + fib2_half1;
+		half2 = fib1_half2 + fib2_half2;
 
-        if (fib1_half2 + fib2_half2 > 9999999999UL)
-        {
-            half1 += 1;
-            half2 %= 10000000000UL;
-        }
+		if (fib1_half2 + fib2_half2 > 9999999999UL)
+		{
+			half1 += 1;
+			half2 %= 10000000000UL;
+		}
 
-        printf("%lu%010lu", half1, half2);
+	printf("%lu%010lu", half1, half2);
 
-        if (count != 98)
-            printf(", ");
+	if (count != 98)
+		printf(", ");
 
-        fib1_half1 = fib2_half1;
-        fib1_half2 = fib2_half2;
-        fib2_half1 = half1;
-        fib2_half2 = half2;
-    }
+	fib1_half1 = fib2_half1;
+	fib1_half2 = fib2_half2;
+	fib2_half1 = half1;
+	fib2_half2 = half2;
+	}
 
-    printf("\n");
+	printf("\n");
 
-    return (0);
+	return (0);
 }
