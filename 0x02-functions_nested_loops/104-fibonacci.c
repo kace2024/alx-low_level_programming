66a964cfb6823362b72baec6640d8a8ec5194630
kace2024 <kacemiabdallah01@gmail.com>
2023-06-21 06:51:41 -0700

    16. In computer class, the first assignment was to write a program to print the first 100 Fibonacci numbers.

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,17 +1,59 @@
-def fibonacci_sequence(n):
-    fib_sequence = []
-    a, b = 1, 2
-    fib_sequence.append(a)
-    fib_sequence.append(b)
-    
-    for i in range(2, n):
-        c = a + b
-        fib_sequence.append(c)
-        a = b
-        b = c
-    
-    return fib_sequence
+#include "main.h"
 
-fibonacci_numbers = fibonacci_sequence(98)
-fibonacci_string = ', '.join(map(str, fibonacci_numbers))
-print(fibonacci_string)
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * print_comma - prints a comma and space
+ */
+void print_comma(void)
+{
+	_putchar(',');
+	_putchar(' ');
+}
+
+/**
+ * print_number - prints a number
+ * @num: The number to print
+ */
+void print_number(unsigned long num)
+{
+	if (num > 9)
+		print_number(num / 10);
+	_putchar((num % 10) + '0');
+}
+
+/**
+ * print_fibonacci - prints the first 98 Fibonacci numbers
+ */
+void print_fibonacci(void)
+{
+	unsigned long first = 1;
+	unsigned long second = 2;
+	unsigned long next;
+	int count;
+
+	print_number(first);
+	print_comma();
+	print_number(second);
+
+	for (count = 3; count <= 98; count++)
+	{
+		next = first + second;
+		print_comma();
+		print_number(next);
+		first = second;
+		second = next;
+	}
+
+	_putchar('\n');
+}
