5b8d4a79d6e9d8c79235f7fb4a762a7e79e1f5e6
kace2024 <kacemiabdallah01@gmail.com>
2023-06-20 17:54:21 -0700

    9. Learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void times_table(void)
+{
+    int i, j, result;
+
+    for (i = 0; i <= 9; i++)
+    {
+        for (j = 0; j <= 9; j++)
+        {
+            result = i * j;
+            if (j == 0)
+            {
+                printf("%2d", result);
+            }
+            else
+            {
+                printf(", %2d", result);
+            }
+        }
+        printf("\n");
+    }
+}
