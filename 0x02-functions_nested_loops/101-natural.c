fed14ac06c785cfce3b116f43e0e0c79d0127947
kace2024 <kacemiabdallah01@gmail.com>
2023-06-20 19:33:37 -0700

    13. Nature made the natural numbers; All else is the work of women

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,10 +1,33 @@
-def sum_multiples_3_or_5(limit):
-    total = 0
-    for num in range(limit):
-        if num % 3 == 0 or num % 5 == 0:
-            total += num
-    return total
+#include <stdio.h>
 
-limit = 1024
-result = sum_multiples_3_or_5(limit)
-print(result)
+int main() {
+    int limit = 1024;
+    int sum = 0;
+
+    for (int i = 1; i < limit; i++) {
+        if (i % 3 == 0 || i % 5 == 0) {
+            sum += i;
+        }
+    }
+
+    // Print the sum using putchar()
+    int temp = sum;
+    int digits = 0;
+
+    // Count the number of digits in the sum
+    while (temp != 0) {
+        temp /= 10;
+        digits++;
+    }
+
+    // Convert each digit of the sum to a character and print it
+    for (int i = digits - 1; i >= 0; i--) {
+        int digit = sum % 10;
+        sum /= 10;
+        putchar(digit + '0');
+    }
+
+    putchar('\n');
+
+    return 0;
+}
