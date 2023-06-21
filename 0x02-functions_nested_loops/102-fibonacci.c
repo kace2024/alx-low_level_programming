c6be3ed807110369b2413994f5ec7f007efa8899
kace2024 <kacemiabdallah01@gmail.com>
2023-06-20 19:29:46 -0700

    14. In computer class

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,23 +1,36 @@
 #include <stdio.h>
 
-void fibonacci(int n) {
-    int i;
-    unsigned long long t1 = 1, t2 = 2, nextTerm;
-
-    printf("Fibonacci Series: ");
-
-    printf("%llu, %llu, ", t1, t2);
-
-    for (i = 3; i <= n; ++i) {
-        nextTerm = t1 + t2;
-        printf("%llu, ", nextTerm);
-        t1 = t2;
-        t2 = nextTerm;
-    }
-    printf("\n");
-}
+void printFibonacci(int n);
 
 int main() {
-    fibonacci(50);
+    printFibonacci(50);
+
     return 0;
+}
+
+void printFibonacci(int n) {
+    int a = 1, b = 2, count = 0;
+
+    // Print the first two Fibonacci numbers
+    putchar(a + '0');
+    putchar(',');
+    putchar(' ');
+    putchar(b + '0');
+    putchar(',');
+    putchar(' ');
+    count += 2;
+
+    // Generate and print the remaining Fibonacci numbers
+    while (count < n) {
+        int next = a + b;
+        putchar(next + '0');
+        putchar(',');
+        putchar(' ');
+
+        a = b;
+        b = next;
+        count++;
+    }
+
+    putchar('\n');
 }
