1763e786a4374e2ff8c6c4c9a631e30b3439b197
kace2024 <kacemiabdallah01@gmail.com>
2023-06-21 13:04:32 -0700

    14. In computer class,

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 
 /**
@@ -5,27 +6,27 @@
  *
  * Return: Nothing!
  */
+
 int main(void)
+
 {
         int i = 0;
 	long j = i, k = 2;
 
         while (i < 50)
         {
-                if (i == 0)
-			printf("%1d, j);
-		else if (i == 1)
-			printf(", %1d", k);
-		else
-                {
-			k += j;
-                        j = k - j;
-			printf(", %1d, k);
-                }
-
-                ++i;
+        if (i == 0)
+	printf("%1d, j);
+	else if (i == 1)
+	printf(", %1d", k);
+	else
+	{
+	k += j;
+	j = k - j;
+	printf(", %1d, k);
         }
-
+        ++i;
+        }
         printf("\n");
         return (0);
 }
