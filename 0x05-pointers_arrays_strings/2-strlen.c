469d294db34850a4c40f3d6285c562f5f6befed0
kace2024 <kacemiabdallah01@gmail.com>
2023-06-26 16:48:17 -0700

    2. This report, by its very length,

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,15 +4,14 @@
  *
  * Return: The length of the string.
  */
-int _strlen(char *s)
-{
-	int length = 0;
+int _strlen(char *s) {
+    int length = 0;
 
-	while (*s != '\0')
-       	{
-		length++;
-		s++;
-	}
+    while (*s != '\0') {
+        length++;
+        s++;
+    }
 
-	return length;
+    return length;
+}
 }
