979756cb5007415d76509248470bc80c3366ae7d
kace2024 <kacemiabdallah01@gmail.com>
2023-06-26 18:16:59 -0700

    9. strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,5 +1,5 @@
 /**
- * _strcpy - Copies the string pointed to by src, including the terminating null byte (\0),
+ * _strcpy - Copies the string pointed to by src,
  *            to the buffer pointed to by dest.
  * @dest: Pointer to the destination buffer.
  * @src: Pointer to the source string.
