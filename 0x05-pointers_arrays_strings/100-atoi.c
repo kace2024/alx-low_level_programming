04f099fb8f692efd3a9d07cc44d29c02d4c838e1
kace2024 <kacemiabdallah01@gmail.com>
2023-06-26 19:07:22 -0700

    10. Great leaders are willing to sacrifice

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,35 +8,39 @@
  */
 int _atoi(char *s)
 {
-	int sign = 1;
-	int result = 0;
-	int i = 0;
+	int i, d, n, len, f, digit;
 
-	if (s[0] == '-')
-	{
-		sign = -1;
-		i++;
-	}
-	else if (s[0] == '+')
-	{
-		i++;
-	}
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
 
-	while (s[i] != '\0')
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
 	{
-		if (s[i] < '0' || s[i] > '9')
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
 		{
-			break;
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9') 
+				break;
+			f = 0;
 		}
-
-		if (result > INT_MAX / 10 || (result == INT_MAX / 10 && (s[i] - '0') > INT_MAX % 10))
-		{
-			return (sign == -1 ? INT_MIN : INT_MAX);
-		}
-
-		result = result * 10 + (s[i] - '0');
 		i++;
 	}
 
-	return sign * result;
+	if (f == 0)
+		return (0);
+
+	return (n);
 }
