f47462427f7cc08989bfe60d781221953b72f078
kace2024 <kacemiabdallah01@gmail.com>
2023-06-26 18:36:30 -0700

    5. A good engineer thinks in reverse and asks

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,17 +6,23 @@
  */
 void rev_string(char *s)
 {
-    if (s == NULL)
-        return;
+	char tmp;
+	int i, len, len1;
 
-    int length = strlen(s);
-    int i, j;
-    char temp;
+	len = 0;
+	len1 = 0;
 
-    for (i = 0, j = length - 1; i < j; i++, j--)
-    {
-        temp = s[i];
-        s[i] = s[j];
-        s[j] = temp;
-    }
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	len1 = len - 1;
+
+	for (i = 0, i <  len / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[len1];
+		s[len1--] = tmp;
+	}
 }
