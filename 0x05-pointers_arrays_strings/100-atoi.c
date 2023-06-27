b6a2eab8f633115b48f5089314e16946370a7824
kace2024 <kacemiabdallah01@gmail.com>
2023-06-26 18:03:13 -0700

    10. Great leaders are willing to sacrifice the numbers to save the people.

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,31 +1,42 @@
+#include <limits.h>
+
 /**
- * _atoi - Converts a string to an integer.
- * @s: The string to be converted.
+ * _atoi - Converts a string to an integer
+ * @s: The string to convert
  *
- * Return: The converted integer.
+ * Return: The converted integer
  */
 int _atoi(char *s)
 {
-    int sign = 1;
-    int result = 0;
-    int i = 0;
+	int sign = 1;
+	int result = 0;
+	int i = 0;
 
-    while (s[i] != '\0')
-    {
-        if (s[i] == '-')
-            sign *= -1;
+	if (s[0] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	else if (s[0] == '+')
+	{
+		i++;
+	}
 
-        if (s[i] >= '0' && s[i] <= '9')
-        {
-            result = result * 10 + (s[i] - '0');
+	while (s[i] != '\0')
+	{
+		if (s[i] < '0' || s[i] > '9')
+		{
+			break;
+		}
 
-            if (s[i + 1] < '0' || s[i + 1] > '9')
-                break;
-        }
+		if (result > INT_MAX / 10 || (result == INT_MAX / 10 && (s[i] - '0') > INT_MAX % 10))
+		{
+			return (sign == -1 ? INT_MIN : INT_MAX);
+		}
 
-        i++;
-    }
+		result = result * 10 + (s[i] - '0');
+		i++;
+	}
 
-    return sign * result;
+	return sign * result;
 }
-
