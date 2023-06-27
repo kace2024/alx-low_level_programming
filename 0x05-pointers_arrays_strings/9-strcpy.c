d1db6eae94fcb71e1b2e62d52f649e4fa99d105a
kace2024 <kacemiabdallah01@gmail.com>
2023-06-26 18:15:31 -0700

    9. strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,12 +8,12 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-    int i;
+	int i;
 
-    for (i = 0; src[i] != '\0'; i++)
-        dest[i] = src[i];
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
 
-    dest[i] = '\0';
+	dest[i] = '\0';
 
-    return dest;
+	return (dest);
 }
