773280ede7d162564ddaba7c3804311308520071
kace2024 <kacemiabdallah01@gmail.com>
2023-06-29 15:38:40 -0700

    11. It is the addition of strangeness to beauty that constitutes the romantic character in art

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,11 +1,12 @@
 #include "main.h"
+
 /**
  * rev_string - reverse array
- * @n: integer parmas
+ * @n: integer params
  * Return: 0
  */
 
-void rev_string(char *n) 
+void rev_string(char *n)
 {
 	int i = 0;
 	int j = 0;
@@ -24,34 +25,35 @@ void rev_string(char *n)
 		*(n + i) = temp;
 	}
 }
+
 /**
- * infinite_add - Adds two numbers represented as strings.
- * @n1: The first number as a string.
- * @n2: The second number as a string.
- * @r: The buffer to store the result.
- * @size_r: The size of the buffer.
- * Return: pointer to calling function.
+ * infinite_add - add 2 numbers together
+ * @n1: text representation of 1st number to add
+ * @n2: text representation of 2nd number to add
+ * @r: pointer to buffer
+ * @size_r: buffer size
+ * Return: pointer to calling function
  */
 
-char *infinite_add(char *n1, char *n2, char *r, int size_r) 
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int overflow = 0, i = 0, j = 0, digits = 0;
-	int vall = 0, temp_tot = 0;
+	int val1 = 0, val2 = 0, temp_tot = 0;
 
-	while (*(n1 +i) != '\0')
+	while (*(n1 + i) != '\0')
 		i++;
-	while (*(n2 +j) != '\0')
+	while (*(n2 + j) != '\0')
 		j++;
 	i--;
-	i--;
+	j--;
 	if (j >= size_r || i >= size_r)
 		return (0);
 	while (j >= 0 || i >= 0 || overflow == 1)
 	{
 		if (i < 0)
-			vall = 0;
+			val1 = 0;
 		else
-			vall = *(n1 + i) - '0';
+			val1 = *(n1 + i) - '0';
 		if (j < 0)
 			val2 = 0;
 		else
@@ -65,12 +67,12 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			return (0);
 		*(r + digits) = (temp_tot % 10) + '0';
 		digits++;
-		j--:
+		j--;
 		i--;
 	}
-	if (digits == size-r)
+	if (digits == size_r)
 		return (0);
 	*(r + digits) = '\0';
-	rev-string(r);
+	rev_string(r);
 	return (r);
 }
