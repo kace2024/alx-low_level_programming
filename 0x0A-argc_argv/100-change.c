e9c32c25085308b72f8bdfa0c846948d34e1bd09
kace2024 <kacemiabdallah01@gmail.com>
2023-07-07 18:13:57 -0700

    kl

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -11,18 +11,7 @@
 */
 int main(int argc, char **argv)
 {
-	int total, count;
-	unsigned int i;
-	char *p;
-	int cents[] = {25, 10, 5, 2};
-
-	if (argc != 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
-
-
+int total, count;
 unsigned int i;
 char *p;
 int cents[] = {25, 10, 5, 2};
