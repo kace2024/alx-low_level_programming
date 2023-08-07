eda5a977ed15f1ce2d59f5493e389c2c7f0e65ff
kacemi <kacemiabdallah01@gmail.com>
2023-08-07 13:03:55 -0700

    3. cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,10 +6,10 @@ char *create_buffer(char *file);
 void close_file(int fd);
 
 /**
- * create_buffer - Allocates 1024 bytes for a buffer.
- * @file: The name of the file buffer is storing chars for.
+ * create_buffer - Allocates a buffer of 1024 bytes.
+ * @file: The name of the file for which the buffer is created.
  *
- * Return: A pointer to the newly-allocated buffer.
+ * Return: A pointer to the newly allocated buffer.
  */
 char *create_buffer(char *file)
 {
@@ -51,10 +51,10 @@ void close_file(int fd)
  *
  * Return: 0 on success.
  *
- * Description: If the argument count is incorrect - exit code 97.
- * If file_from does not exist or cannot be read - exit code 98.
- * If file_to cannot be created or written to - exit code 99.
- * If file_to or file_from cannot be closed - exit code 100.
+ * Description: If the argument count is incorrect, the program will exit with code 97.
+ * If the file_from does not exist or cannot be read, the program will exit with code 98.
+ * If the file_to cannot be created or written to, the program will exit with code 99.
+ * If either the file_to or file_from cannot be closed, the program will exit with code 100.
  */
 int main(int argc, char *argv[])
 {
