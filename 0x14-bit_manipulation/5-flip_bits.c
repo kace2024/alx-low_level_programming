ea12c01ee3840982aba234a216dd8dab4fd8d9ff
kacemi abdallah <kacemiabdallah01@gmail.com>
2023-08-31 14:09:28 -0700

    tasks 5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -5,20 +5,16 @@
  * @n: the first number.
  * @m: the second number.
  *
- * Return: number of bits to change
+ * Return: the number of bits to flip.
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	int i, count = 0;
-	unsigned long int current;
-	unsigned long int exclusive = n ^ m;
-
-	for (i = 63; i >= 0; i--)
-	{
-		current = exclusive >> i;
-		if (current & 1)
+	unsigned int count = 0;
+	while (n || m) {
+		if ((n & 1) != (m & 1))
 			count++;
+		n >>= 1;
+		m >>= 1;
 	}
-
 	return (count);
 }
