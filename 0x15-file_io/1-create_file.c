d29e9ac9dc8093564510bc60b70aaee843dc1cff
kacemi abdallah <kacemiabdallah01@gmail.com>
2023-09-04 13:36:57 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,16 +12,24 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-    int fd;
-    if (filename == NULL)
-        return (-1);
-    fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-    if (fd == -1)
-        return (-1);
-    if (text_content != NULL)
-    {
-        write(fd, text_content, strlen(text_content));
-    }
-    close(fd);
-    return (1);
+    int fd, w, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
+
+	if (fd == -1 || w == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
 }
