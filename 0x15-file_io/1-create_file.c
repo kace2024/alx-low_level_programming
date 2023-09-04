a65548a3d73aa2cdd030f15db83b0af37d9a5478
kacemi abdallah <kacemiabdallah01@gmail.com>
2023-09-04 13:39:34 -0700

    1. Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,7 +8,7 @@
  * @filename: The name of the file to create.
  * @text_content: The text content to write to the file.
  *
- * Return: 1 on success, -1 on failure.
+ * Return: 1 on success, - -1 on failure.
  */
 int create_file(const char *filename, char *text_content)
 {
